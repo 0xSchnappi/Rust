89ead59afad205279a623a03d4872e074c9a497a
0xSchnappi <952768182@qq.com>
2024-06-29 17:34:03 +0800

    所有权

--- src/main.rs
+++ src/main.rs
@@ -421,7 +421,11 @@ fn data_type() {
     println!("{:?}", a);
     let c = "hello".to_string();
     let d = c; // Rust使用的是move，hello的内存绑定到变量d上，之前c上的绑定就不能使用了，c无法再用
-    // println!("c: {:?}", c);
+               // println!("c: {:?}", c);
+               /* 重要：copy另一个使用就是不可变引用(&T)，但是注意可变引用(&mut T)是不可以Copy的 */
+    let x: &str = "hello, world";
+    let y = x;
+    println!("{},{}", x, y);
 
     let e = G;
     e.hello();
@@ -442,9 +446,61 @@ fn data_type() {
      * 同理foo("hello")将被转换如下
      * fn foo_2(x: &'static str) -> &'static str{
      *     return x;
-     * } 
+     * }
      */
 }
+
+// 所有权
+fn takes_ownership(some_string: String) {
+    // some_string 进入作用域
+    println!("{}", some_string);
+} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+
+fn makes_copy(some_integer: i32) {
+    // some_integer 进入作用域
+    println!("{}", some_integer);
+} // 这里，some_integer 移出作用域。不会有特殊操作
+
+fn gives_ownership() -> String {
+    // gives_ownership 将返回值移动给
+    // 调用它的函数
+
+    let some_string = String::from("hello"); // some_string 进入作用域.
+
+    some_string // 返回 some_string 并移出给调用的函数
+}
+
+// takes_and_gives_back 将传入字符串并返回该值
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string 进入作用域
+
+    a_string // 返回 a_string 并移出给调用的函数
+}
+
+fn ownership() {
+    let s = String::from("hello"); // s 进入作用域
+
+    takes_ownership(s); // s 的值移动到函数里 ...
+                        // ... 所以到这里不再有效
+
+    let x = 5; // x 进入作用域
+
+    makes_copy(x); // x 应该移动函数里，
+                   // 但 i32 是 Copy 的，所以在后面可继续使用 x
+
+    let s1 = gives_ownership(); // gives_ownership 将返回值
+                                // 移给 s1
+
+    let s2 = String::from("hello"); // s2 进入作用域
+
+    let s3 = takes_and_gives_back(s2); // s2 被移动到
+                                       // takes_and_gives_back 中,
+                                       // 它也将返回值移给 s3
+
+} // 这里, x 先移出了作用域，然后是 s。但因为 s 的值已被移走，
+  // 所以不会有特殊操作
+  // 这里, s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，
+  // 所以什么也不会发生。s1 移出作用域并被丢弃
 
 fn main() {
     hellworld();
@@ -464,7 +520,7 @@ fn main() {
     for i in RangeInclusive::new(1, 5) {
         print!("{}", i);
     }
-    // https://course.rs/basic/base-type/statement-expression.html
 
     data_type();
+    ownership();
 }

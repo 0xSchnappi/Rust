aa00ef18fd04cde4483124287b1ac2cb76565f0f
0xSchnappi <952768182@qq.com>
2024-07-15 16:08:23 +0800

    线程同步:消息传递

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fmt;
 /// 文档注释
 /// 稳定注释
 use std::fmt::Display;
+use std::sync::mpsc::sync_channel;
 
 // 有理数和复数社区库，没有标准库
 use num::complex::Complex;
@@ -2567,6 +2568,11 @@ fn auto_ptr() {
 
 use std::sync::Barrier;
 use std::time::Duration;
+
+enum Fruit {
+    Apple(u8),
+    Orange(String),
+}
 fn multiple_thread() {
     let v = vec![1, 2, 3];
 
@@ -2714,7 +2720,7 @@ fn multiple_thread() {
     });
 
     // 在主线程中接收子线程发送的消息并输出
-    println!("receive {}", rx.recv().unwrap());     // 阻塞线程直到读取到值或者通道被关闭
+    println!("receive {}", rx.recv().unwrap()); // 阻塞线程直到读取到值或者通道被关闭
 
     // 不阻塞方法
 
@@ -2760,6 +2766,76 @@ fn multiple_thread() {
     }
 
     // 使用多发送者
+    // 由于子线程拿走发送者的所有权，所以需要克隆
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        tx.send(String::from("hi from raw tx")).unwrap();
+    });
+
+    thread::spawn(move || {
+        tx1.send(String::from("hi from cloned tx")).unwrap();
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    // 同步通道
+
+    let (tx, rx) = mpsc::sync_channel(0);
+
+    let hande = thread::spawn(move || {
+        println!("发生之前");
+        tx.send(1).unwrap();
+        println!("发生之后");
+    });
+
+    println!("睡眠之前");
+    thread::sleep(Duration::from_secs(3));
+    println!("睡眠之后");
+
+    println!("receive {}", rx.recv().unwrap());
+    hande.join().unwrap();
+    // 使用异步消息虽然能非常高效且不会造成发送线程的阻塞，但是存在消息未及时消费，最终内存过大的问题。
+    // 在实际项目中，可以考虑使用一个带缓冲值的同步通道来避免这种风险。
+
+    // 消息通道传输多种类型的数据
+    use std::sync::mpsc::{Receiver, Sender};
+    let (tx, rx): (Sender<Fruit>, Receiver<Fruit>) = mpsc::channel();
+
+    tx.send(Fruit::Orange("sweet".to_string())).unwrap();
+    tx.send(Fruit::Apple(2)).unwrap();
+
+    for _ in 0..2 {
+        match rx.recv().unwrap() {
+            Fruit::Apple(count) => println!("received {} apples", count),
+            Fruit::Orange(flavor) => println!("receiived {} oranges", flavor),
+        }
+    }
+
+    // 新手坑
+    println!("新手坑");
+    let (send, recv) = mpsc::channel();
+    let num_threads = 3;
+    for i in 0..num_threads {
+        let thread_send = send.clone();
+        thread::spawn(move || {
+            thread_send.send(i).unwrap();
+            println!("thread {:?} finished", i);
+        });
+    }
+
+    // 在这里drop send...
+    drop(send); // 如果不drop send，这个send通道一直在打开状态，下面的循环则会不断循环获取
+
+    for x in recv {
+        println!("Got: {}", x);
+    }
+    println!("finished iterating");
+
+    // 性能更好的多发送多接收
+    // crossbeam-channel、flume
 }
 
 fn advanced_parctice() {
